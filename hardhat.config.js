80c91297d8b854a78421e28c94abbe6f41028445
kihiuFrank <franklinekihiu@gmail.com>
2023-03-30 04:13:57 +0300

    deloyment to goerli  bug fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,83 +1,68 @@
 require("@nomicfoundation/hardhat-toolbox")
-require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
 require("dotenv").config()
 require("solidity-coverage")
-require("hardhat-deploy")
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+//require("@nomiclabs/hardhat-ethers")
 
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
+/** @type import('hardhat/config').HardhatUserConfig */
+
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const KOVAN_RPC_URL =
+    process.env.KOVAN_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
-    process.env.PRIVATE_KEY || "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
+    process.env.PRIVATE_KEY ||
+    "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
-
 module.exports = {
+    //solidity: "0.8.7",
+    solidity: {
+        compilers: [
+            { version: "0.8.7" },
+            { version: "0.6.6" },
+            { version: "0.8.8" },
+        ],
+    },
     defaultNetwork: "hardhat",
     networks: {
-        hardhat: {
-            chainId: 31337,
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
-        },
-        localhost: {
-            chainId: 31337,
+        rinkeby: {
+            url: RINKEBY_RPC_URL || "",
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
         goerli: {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
-            saveDeployments: true,
             blockConfirmations: 6,
-            gas: 6000000,
         },
-    },
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.8",
-            },
-            {
-                version: "0.6.12",
-            },
-            {
-                version: "0.6.6",
-            },
-            {
-                version: "0.6.0",
-            },
-            {
-                version: "0.4.19",
-            },
-        ],
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            //accounts: Hardhat will sort
+            chainId: 31337,
+        },
     },
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
-        customChains: [], // fixing TypeError: customChains is not iterable
     },
     gasReporter: {
         enabled: true,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
     },
     namedAccounts: {
         deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer.
-            //Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
-        player: {
-            default: 1,
+            default: 0,
         },
     },
 }
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,34 +1,25 @@
 const networkConfig = {
-    31337: {
-        name: "localhost",
-        ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
-        mintFee: "10000000000000000", // 0.01 ETH
-        callbackGasLimit: "500000", // 500,000 gas
+    4: {
+        name: "rinkeby",
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e", //0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
     },
-    // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
     5: {
         name: "goerli",
         ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
-        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
-        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        callbackGasLimit: "500000", // 500,000 gas
-        mintFee: "10000000000000000", // 0.01 ETH or ethers.utils.parseEther("0.01"),
-        subscriptionId: "1996", // add your ID here!
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
     },
 }
 
-const DECIMALS = "18"
-const INITIAL_PRICE = "200000000000000000000"
 const developmentChains = ["hardhat", "localhost"]
-const frontEndContractFile = "../nextjs-nft-marketplace-thegraph/constants/networkMapping.json"
-const frontEndAbiLocation = "../nextjs-nft-marketplace-thegraph/constants/"
+const DECIMALS = 8
+const INITIAL_ANSWER = 150000000000
 
 module.exports = {
     networkConfig,
     developmentChains,
     DECIMALS,
-    INITIAL_PRICE,
-    frontEndContractFile,
-    frontEndAbiLocation,
+    INITIAL_ANSWER,
 }
